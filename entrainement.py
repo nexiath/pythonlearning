b835ef325e25949732668d06be37aa5ac40b5579
Robin Cassard <robin.cassard@edu.univ-fcomte.fr>
2023-10-12 10:17:22 +0200

    on continue

--- entrainement.py
+++ entrainement.py
@@ -1,5 +1,17 @@
-import sys
-print("version de python")
-print(sys.version)
-print("+ d'info")
-print(sys.version_info)
+# import sys
+# print("version de python")
+# print(sys.version)
+# print("+ d'info")
+# print(sys.version_info)
+
+
+# from datetime import date
+
+# today = date.today()
+# print("La date d'aujourd'hui est : ", today)
+
+# from math import *
+
+# r=1.1
+# aire = pi*r*r
+# print(aire)
