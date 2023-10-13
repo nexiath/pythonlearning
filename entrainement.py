1d3a73e9e0ad496cc2a40384baef2b74ade61db9
nexiath <108474515+nexiath@users.noreply.github.com>
2023-10-13 14:25:43 +0200

    Update entrainement.py

--- entrainement.py
+++ entrainement.py
@@ -4,14 +4,11 @@
 # print("+ d'info")
 # print(sys.version_info)
 
-
 # from datetime import date
-
 # today = date.today()
 # print("La date d'aujourd'hui est : ", today)
 
 # from math import *
-
 # r=1.1
 # aire = pi*r*r
 # print(aire)
